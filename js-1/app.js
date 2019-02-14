a834b579adf00e8cec99e52c65d572251ccbe611
alexscub <alexscub1@gmail.com>
2019-02-15 00:57:53 +0200

    JS-1

---
+++ js-1/app.js
@@ -0,0 +1,25 @@
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+const cancelLogin = 'Отменено пользователем!';
+const wrongLogin = 'Доступ запрещен, неверный логин!';
+const cancelPassword = 'Отменено пользователем!';
+const wrongPassword = 'Доступ запрещен, неверный пароль!';
+const truePassword = 'Добро пожаловать!';
+let login = prompt('Введите логин!');
+//проверяем логин
+if (login === adminLogin) {
+  //если логин совпал, просим пароль
+  let password = prompt('Введите пароль!')
+  //проверяем пароль
+  if (password === adminPassword) {
+    alert(truePassword)
+  } else if (password === null) {
+    alert(cancelPassword)
+  } else {
+    alert(wrongPassword)
+  }
+} else if (login === null) {
+  alert(cancelLogin)
+} else {
+  alert(wrongLogin)
+}
